5fc311a256277926d9985409e64625447e52f3b6
Eric Yabit <eric.yabit@live.com>
2019-06-25 03:08:20 +0000

    Initial commit

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :destroy]
 
   def index
   @places = Place.paginate(page: params[:page]).per_page(4)
@@ -7,11 +7,7 @@ class PlacesController < ApplicationController
   def new
     @place = Place.new
   end
-  
-  def create
-    current_user.places.create(place_params)
-    redirect_to root_path
-  end
+    
 
 def show
   @place = Place.find(params[:id])
@@ -27,10 +23,38 @@ def edit
 end
 
 def update
-    @place = Place.find(params[:id])
-    @place.update_attributes(place_params)
-    redirect_to root_path
+  @place = Place.find(params[:id])
 
+  if @place.user != current_user
+    return render plain: 'Not Allowed', status: :forbidden
+  end
+
+  @place.update_attributes(place_params)
+  if @place.valid?
+    redirect_to root_path
+  else
+    render :edit, status: :unprocessable_entity
+  end
+end
+
+
+def destroy
+  @place = Place.find(params[:id])
+  if @place.user != current_user
+    return render plain: 'Not Allowed', status: :forbidden
+  end
+
+  @place.destroy
+  redirect_to root_path
+end
+
+  def create
+  @place = current_user.places.create(place_params)
+  if @place.valid?
+    redirect_to root_path
+  else
+    render :new, status: :unprocessable_entity
+  end
 end
 
   private
