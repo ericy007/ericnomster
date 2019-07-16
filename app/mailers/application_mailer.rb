0417d9d2e2d8dfb7799c2ed61671f1b15c75e1ba
Your Name <you@example.com>
2019-07-16 03:12:23 +0000

    Picture upload

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,12 +2,4 @@ class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
 
-  def account_activation(user)
-    @user = user
-    mail to: user.email, subject: "Account activation"
-    end
-  def password_reset(user)
-    @user = user
-    mail to: user.email, subject: "Password reset"
-  end
 end
