a7f662ccb5c95cca43d90ddd73ba1bed8eee9aaf
Eric Yabit <eric.yabit@live.com>
2019-07-16 03:41:05 +0000

    removed passwword reset from user table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_10_031226) do
+ActiveRecord::Schema.define(version: 2019_07_16_033818) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,8 +57,6 @@ ActiveRecord::Schema.define(version: 2019_07_10_031226) do
     t.datetime "updated_at", null: false
     t.string "reset_digest"
     t.datetime "reset_sent_at"
-    t.string "password_reset_token"
-    t.datetime "password_reset_sent_at"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
