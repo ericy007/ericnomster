376f45eef9b6b9937b14a4f9fbd80b2e929b10f4
Eric Yabit <eric.yabit@live.com>
2019-07-16 03:49:56 +0000

    removed passwword reset from user table

---
+++ db/migrate/20190716034734_remove_reset_digest_from_users.rb
@@ -0,0 +1,6 @@
+class RemoveResetDigestFromUsers < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :users, :reset_digest, :string
+    remove_column :users, :reset_sent_at, :datetime
+  end
+end
