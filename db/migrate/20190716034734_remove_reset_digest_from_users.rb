00bce5c491beef1f3639777afeab38a7f5ddb1c5
Your Name <you@example.com>
2019-07-29 04:21:09 +0000

    Adding the Places a User Created

--- db/migrate/20190716034734_remove_reset_digest_from_users.rb
+++
@@ -1,6 +0,0 @@
-class RemoveResetDigestFromUsers < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :users, :reset_digest, :string
-    remove_column :users, :reset_sent_at, :datetime
-  end
-end
