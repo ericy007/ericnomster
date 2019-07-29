60d2c1baea7d5e3e81e64c8a945e9d63215e8eec
Eric Yabit <eric.yabit@live.com>
2019-07-29 04:21:09 +0000

    Adding the Places a User Created

--- db/migrate/20190716033818_remove_password_reset_from_users.rb
+++
@@ -1,6 +0,0 @@
-class RemovePasswordResetFromUsers < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :users, :password_reset_token, :string
-    remove_column :users, :password_reset_sent_at, :datetime
-  end
-end
